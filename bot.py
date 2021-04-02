a458f274decc7232e97a313a77500b4cf9815d58
KCsup <kcsup05@gmail.com>
2021-04-02 18:44:20 -0400

    Added 'riot.txt'.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 import valorant
 import riotwatcher
-from riotwatcher import LolWatcher, ApiError, ValWatcher
+from riotwatcher import LolWatcher, ApiError, ValWatcher, RiotWatcher
 import util.common as common
 import os
 
@@ -19,21 +19,28 @@ data = json.load(file)
 
 client.remove_command('help')
 
-# vClient = valorant.Client(vKey)
-# acc = vClient.get_user_by_name('KCsup#2216', '#')
-# for x in vClient.get_leaderboard().players:
-#     print(x.gameName)
-# print(acc.puuid)
-print('hi')
-
 rKey = data['riotToken']
 lol_watcher = LolWatcher(rKey)
 region = common.region
-s = lol_watcher.summoner.by_name(region, 'aceofspades735')
-ml = lol_watcher.match.matchlist_by_account(region=region, encrypted_account_id=s['accountId'], begin_index=0, end_index=1)
-cID = ml['matches'][0]['champion']
-print(ml)
-print(common.getChampion(cID))
+# s = lol_watcher.summoner.by_name(region, 'kcsup05')
+# ml = lol_watcher.match.matchlist_by_account(region=region, encrypted_account_id=s['accountId'], begin_index=0, end_index=1)
+# cID = ml['matches'][0]['champion']
+# print(ml)
+# print(common.getChampion(cID))
+
+# vClient = valorant.Client(rKey)
+# acc = vClient.get_user_by_name('KCsup#2216', '#')
+# print(acc)
+# print('hi')
+
+# r_watcher = RiotWatcher(data['dToken'])
+# player = r_watcher.account.by_riot_id(region='AMERICAS', game_name='KCsup', tag_line='2216')
+# print(player)
+
+# print(s['puuid'])
+# val_watcher = ValWatcher(rKey)
+# v = val_watcher.match.matchlist_by_puuid(region='NA', puuid=player['puuid'])
+# print(v)
 
 # my_region = 'na1'
 # me = None
